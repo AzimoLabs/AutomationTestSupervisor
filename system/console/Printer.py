35f86a60acb688e030d0834e7f736748aaf6a066
Kamil Krzyk <kamil.krzyk@azimo.com>
2017-05-24 16:00:17 +0200

    console output format adjustments

--- system/console/Printer.py
+++ system/console/Printer.py
@@ -17,13 +17,13 @@ def step(message):
 
 
 def system_message(tag, message):
-    print(Color.BLUE + '[{:%H:%M:%S}]   - '.format(datetime.datetime.now()) + tag + " " + message + Color.END)
+    print(Color.BLUE + '[{:%H:%M:%S}] - '.format(datetime.datetime.now()) + tag + " " + message + Color.END)
 
 
 def console_highlighted(tag, message, cmd):
-    print(Color.BLUE + '[{:%H:%M:%S}]   - '.format(datetime.datetime.now())
+    print(Color.BLUE + '[{:%H:%M:%S}] - '.format(datetime.datetime.now())
           + tag + " " + message + Color.GOLD + cmd + Color.END)
 
 
 def console(message, end):
-    print(Color.DARK_PURPLE + '[{:%H:%M:%S}]       > '.format(datetime.datetime.now()) + message + Color.END, end=end)
+    print(Color.DARK_PURPLE + '[{:%H:%M:%S}]   > '.format(datetime.datetime.now()) + message + Color.END, end=end)
